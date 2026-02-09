7c54499866f64b6b2a8a148fe9888c784dadeca2
Rohit Mathur <116487107+Rohit0265@users.noreply.github.com>
2026-02-09 09:25:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("three")) return "three";
+            if (id.includes("gsap")) return "gsap";
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
+});
