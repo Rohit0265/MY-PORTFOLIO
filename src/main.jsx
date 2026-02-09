7688b176bbbe6b18d6cf15a03be47c7f72a9524c
Rohit Mathur <116487107+Rohit0265@users.noreply.github.com>
2026-02-09 08:21:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React>
+  <React.StrictMode>
     <App />
-  </React>
+  </React.StrictMode>
 );
